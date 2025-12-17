5ccb7bebca737d98b26a46d272f5adeea7bb1dc4
kaxadeveloper <kaxa3developer@yahoo.com>
2025-12-17 21:59:11 +0400

    jquery

---
+++ app.js
@@ -0,0 +1,14 @@
+$('.add-btn').click(function () {
+    $('ul').append("<li><span><i class='fa fa-trash'></i></span>" + $('input').val() + "</li>");
+    $('input').val("");
+});
+
+$('ul').on("click", 'span', function () {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+});
+
+$('.icon').click(function(){
+    $('.field').toggleClass("hide");
+})
